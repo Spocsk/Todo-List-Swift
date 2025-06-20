1e7f9a442d8713119030af926e3ec6279b6fa318
Dylan COUTO DE OLIVEIRA <dylandasilva79@gmail.com>
2025-06-20 22:47:45 +0200

    Initial Commit

---
+++ Todo-List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo-List
+//
+//  Created by Dylan COUTO DE OLIVEIRA on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
